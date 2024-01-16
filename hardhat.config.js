18401c25d457230545829b8c9d7b9c8f07e4e7ca
ChaitanyaKampara <136173270+ChaitanyaKampara@users.noreply.github.com>
2024-01-16 12:57:44 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.4",
   networks:{
-    hardhat:{},
+    hardhat:{}, 
     sepolia:{
       url: "https://eth-sepolia.g.alchemy.com/v2/uLj_olWrBwOBssNM27S84v5DEC0u1R_F",
       accounts:[`0x${"a20b4c417b249247883975a89aa8ab7716fb9c40d74bdf850c173c316c96d35e"}`,],
